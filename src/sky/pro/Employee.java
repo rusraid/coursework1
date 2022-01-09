ce6f6178b4dcbb318d033dde590795af9588403a
Evgrafov Ruslan <rusraid@mail.ru>
2022-01-09 19:39:56 +0400

    1 commmit

--- src/sky/pro/Employee.java
+++ src/sky/pro/Employee.java
@@ -1,4 +1,91 @@
 package sky.pro;
 
+import java.util.Objects;
+
 public class Employee {
+    private static int counter = 1;
+
+    private int id;
+    private final String firstName;
+    private final String lastName;
+    private final String middleName;
+    private int department;
+    private int salary;
+
+    public Employee(String firstName, String lastName, String middleName, int department, int salary) {
+
+        this.id = counter++;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.middleName = middleName;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public static int getCounter() {
+        return counter;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getFullName() {
+        return firstName + " " + lastName + " " + middleName;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", department=" + department +
+                ", salary=" + salary +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id && department == employee.department && salary == employee.salary && Objects.equals(firstName, employee.firstName) && Objects.equals(lastName, employee.lastName) && Objects.equals(middleName, employee.middleName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, middleName, department, salary);
+    }
+
 }
